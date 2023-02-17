3e3e146dc3b9ec5442a9a07bc4e86045ef512e7e
Harshit Jain <dev-harsh1998@hotmail.com>
2023-02-18 04:47:50 +0530

    nabu: parts: completely use new hwcontrol hal for everything
    * Remove xiaomi touchfeature.
    * Rearrange code a bit.
    
    Signed-off-by: DragonEmperor9480 <amruteshnaregal1234@gmail.com>

--- parts/src/org/lineageos/settings/utils/PeripheralUtils.java
+++ parts/src/org/lineageos/settings/utils/PeripheralUtils.java
@@ -5,12 +5,8 @@ import android.content.SharedPreferences;
 import android.os.UserHandle;
 import android.util.Log;
 
-import vendor.xiaomi.hardware.touchfeature.V1_0.ITouchFeature;
-
-import custom.hardware.hwcontrol.IHwControl;
+import org.lineageos.settings.hwcontrol.HwStateManager;
 import custom.hardware.hwcontrol.HwType;
-import android.os.ServiceManager;
-import android.os.IBinder;
 
 import static org.lineageos.settings.stylus.StylusSettingsFragment.SHARED_STYLUS;
 import static org.lineageos.settings.keyboard.XiaomiKeyboardSettingsFragment.SHARED_KEYBOARD;
@@ -18,9 +14,7 @@ import static org.lineageos.settings.keyboard.XiaomiKeyboardSettingsFragment.SHA
 public class PeripheralUtils {
     private static final String TAG = "PeripheralUtils";
     private static final boolean DEBUG = false;
-    private static final String IHWCONTROL_AIDL_INTERFACE = "custom.hardware.hwcontrol.IHwControl/default";
-    private static IHwControl mHwControl;
-    private static ITouchFeature mTouchFeature;
+    private static HwStateManager mHwStateManager;
     private static SharedPreferences stylus;
     private static SharedPreferences keyboard;
 
@@ -29,25 +23,10 @@ public class PeripheralUtils {
         // Initialize stylus and keyboard shared preferences
         stylus = context.getSharedPreferences(SHARED_STYLUS, Context.MODE_PRIVATE);
         keyboard = context.getSharedPreferences(SHARED_KEYBOARD, Context.MODE_PRIVATE);
+        tap2wake = context.getSharedPreferences(SHARED_TAP2WAKE, Context.MODE_PRIVATE);
 
-        // try to get the touchfeature service.
-        try {
-            mTouchFeature = ITouchFeature.getService();
-        } catch (Exception e) {
-            Log.e(TAG, "Failed to get touchfeature service", e);
-        }
-
-        IBinder binder = ServiceManager.getService(IHWCONTROL_AIDL_INTERFACE);
-        if (binder == null) {
-            Log.e(TAG, "Getting " + IHWCONTROL_AIDL_INTERFACE + " service daemon binder failed!");
-        } else {
-            mHwControl = IHwControl.Stub.asInterface(binder);
-            if (mHwControl == null) {
-                Log.e(TAG, "Getting IHwControl AIDL daemon interface failed!");
-            } else {
-                Log.d(TAG, "Getting IHwControl AIDL interface binding success!");
-            }
-        }
+        // Initialize HwStateManager
+        mHwStateManager.BootResetState();
 
         // Sync all peripherals
         SyncAll();
@@ -56,27 +35,19 @@ public class PeripheralUtils {
     // Enable stylus based on shared preference.
     private static void SyncStylus() {
         if (DEBUG) Log.d(TAG, "Enabling stylus");
-        if (mTouchFeature != null) {
-            try {
-                mTouchFeature.setTouchMode(20, stylus.getInt(SHARED_STYLUS, 0));
-            } catch (Exception e) {
-                Log.e(TAG, "Failed to enable stylus", e);
-            }
-        }
+        mHwStateManager.HwState(HwType.STYLUS, stylus.getInt(SHARED_STYLUS, 0));
     }
 
     // Enable keyboard based on shared preference.
     private static void SyncKeyboard() {
         if (DEBUG) Log.d(TAG, "Enabling keyboard");
-        if (mHwControl != null) {
-            try {
-                mHwControl.setHwState(HwType.KEYBOARD, keyboard.getInt(SHARED_KEYBOARD, 0));
-            } catch (Exception e) {
-                Log.e(TAG, "Failed to enable keyboard", e);
-            }
-        } else {
-            Log.d(TAG, "mHwControl is null");
-        }
+        mHwStateManager.HwState(HwType.KEYBOARD, keyboard.getInt(SHARED_KEYBOARD, 0));
+    }
+
+    // Enable tap2wake based on shared preference.
+    public static void SyncTap2Wake() {
+        if (DEBUG) Log.d(TAG, "Enabling tap2wake");
+        mHwStateManager.HwState(HwType.TAP2WAKE, tap2wake.getInt(SHARED_TAP2WAKE, 0));
     }
 
     // Sync all peripherals
